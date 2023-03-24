13d8f8c2fb46ae148142c0b0e5c1deb48fe00137
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-03-24 16:14:23 +0100

    feat: navbar completed

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  data() {
+    return {
+      linksList: [
+        "CHARACTERS",
+        "COMICS",
+        "MOVIES",
+        "TV",
+        "GAMES",
+        "COLLECTIBLES",
+        "VIDEOS",
+        "FANS",
+        "NEWS",
+        "SHOP",
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div
+    id="navbar-container"
+    class="d-flex justify-content-between align-items-center"
+  >
+    <div class="img-container">
+      <img src="/img/dc-logo.png" alt="DC Logo" />
+    </div>
+    <ul class="d-flex align-items-center">
+      <li v-for="link in linksList">{{ link }}</li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#navbar-container {
+  max-width: 1200px;
+  margin: 0 auto;
+
+  .img-container {
+    padding: 1em 0;
+  }
+}
+
+ul {
+  gap: 20px;
+  font-weight: bold;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.scss";
+import App from "./App.vue";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "../node_modules/bootstrap/scss/bootstrap.scss";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
